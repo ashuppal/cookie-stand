0ed6375cee3565ac765024b59c91ffdc46b9eed7
Ash Uppal <ashuppal@gmail.com>
2022-11-01 00:19:55 -0700

    added .eslintrc.json

--- js/app.js
+++ js/app.js
@@ -3,17 +3,32 @@
 // #pragma: GLOBAL VARIABLES + WINDOW INTO DOM
 // FOR LAB CREATE A GLOBAL ARRAY WITH SHOP HOURS
 
-let shopHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm','7pm','8pm'];
+let shopHours = [
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12am',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '6pm',
+  '7pm',
+  '8pm'
+];
 
 let cookieAvg = [];
 
-function randomCookie(){
-  for(let i = 0; i<shopHours.length; i++){
-    let randomCookie = Math.floor(Math.random() *200);
+function randomCookie () {
+  for (let i = 0; i < shopHours.length; i++) {
+    let randomCookie = Math.floor(Math.random() * 200);
     cookieAvg.push(randomCookie);
   }
 }
-
 
 //**  DOM MANIPULATION STEP 1: WINDOW INTO THE DOM
 
@@ -21,36 +36,32 @@ let shopSection = document.getElementById('shopProfiles');
 
 console.dir(shopSection);
 
-
 // #pragma: HELPER FUNCTIONS - UTILITIES
 
 // grabbed from MDN docs
-function randomMinCust(min, max) {
+function randomMinCust (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-function randomMaxCust(min, max) {
+function randomMaxCust (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 // #pragma: OBJECT LITERALS
 
 let seattleShop = {
-
   location: 'Seattle',
   minCustomer: 0,
-  maxCustomer:0,
+  maxCustomer: 0,
   avgCookieSale: 0,
-  cust1: randomMinCust(1,100),
-  cust2:randomMaxCust(1,100),
+  cust1: randomMinCust(1, 100),
+  cust2: randomMaxCust(1, 100),
 
-  render:function(){
-
+  render: function () {
     randomCookie();
 
-    if(this.cust1 > this.cust2){
+    if (this.cust1 > this.cust2) {
       this.maxCustomer = this.cust1;
       this.minCustomer = this.cust2;
-    }
-    else{
+    } else {
       this.maxCustomer = this.cust2;
       this.minCustomer = this.cust1;
     }
@@ -65,6 +76,58 @@ let seattleShop = {
 
     shopSection.appendChild(articleElem);
 
+    //create h2 element
+
+    let h2Elem = document.createElement('h2');
+    h2Elem.textContent = this.location;
+    articleElem.appendChild(h2Elem);
+
+    //create unordered list
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+
+    for (let i = 0; i < shopHours.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = `${shopHours[i]}: ${cookieAvg[i]} cookies`;
+      ulElem.appendChild(liElem);
+    }
+  }
+};
+
+seattleShop.render();
+
+
+
+
+let tokyoShop = {
+  location: 'Tokyo',
+  minCustomer: 0,
+  maxCustomer: 0,
+  avgCookieSale: 0,
+  cust1: randomMinCust(1, 80),
+  cust2: randomMaxCust(1, 100),
+
+  render: function () {
+    randomCookie();
+
+    if (this.cust1 > this.cust2) {
+      this.maxCustomer = this.cust1;
+      this.minCustomer = this.cust2;
+    } else {
+      this.maxCustomer = this.cust2;
+      this.minCustomer = this.cust1;
+    }
+    // ********* DOM MANIPULATION **********
+    // ** STEP 2: CREATE THE ELEMENT
+
+    let articleElem = document.createElement('article');
+    // ** STEP 3: GIVE CONTEXT IF NECESSARY
+    // skip for articleElem
+
+    // ** STEP 4: ADD IT TO THE DOM ---> parent.appendChild(child)
+
+    shopSection.appendChild(articleElem);
 
     //create h2 element
 
@@ -72,20 +135,166 @@ let seattleShop = {
     h2Elem.textContent = this.location;
     articleElem.appendChild(h2Elem);
 
+    //create unordered list
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+
+    for (let i = 0; i < shopHours.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = `${shopHours[i]}: ${cookieAvg[i]} cookies`;
+      ulElem.appendChild(liElem);
+    }
+  }
+};
+tokyoShop.render();
+
+
+let dubaiShop = {
+  location: 'Dubai',
+  minCustomer: 0,
+  maxCustomer: 0,
+  avgCookieSale: 0,
+  cust1: randomMinCust(1, 80),
+  cust2: randomMaxCust(1, 100),
+
+  render: function () {
+    randomCookie();
+
+    if (this.cust1 > this.cust2) {
+      this.maxCustomer = this.cust1;
+      this.minCustomer = this.cust2;
+    } else {
+      this.maxCustomer = this.cust2;
+      this.minCustomer = this.cust1;
+    }
+    // ********* DOM MANIPULATION **********
+    // ** STEP 2: CREATE THE ELEMENT
+
+    let articleElem = document.createElement('article');
+    // ** STEP 3: GIVE CONTEXT IF NECESSARY
+    // skip for articleElem
+
+    // ** STEP 4: ADD IT TO THE DOM ---> parent.appendChild(child)
+
+    shopSection.appendChild(articleElem);
+
+    //create h2 element
+
+    let h2Elem = document.createElement('h2');
+    h2Elem.textContent = this.location;
+    articleElem.appendChild(h2Elem);
 
     //create unordered list
 
     let ulElem = document.createElement('ul');
     articleElem.appendChild(ulElem);
 
-    for(let i = 0; i<shopHours.length; i++){
+    for (let i = 0; i < shopHours.length; i++) {
       let liElem = document.createElement('li');
       liElem.textContent = `${shopHours[i]}: ${cookieAvg[i]} cookies`;
       ulElem.appendChild(liElem);
     }
-
-
   }
 };
+dubaiShop.render();
 
-seattleShop.render();
+
+let parisShop = {
+  location: 'Paris',
+  minCustomer: 0,
+  maxCustomer: 0,
+  avgCookieSale: 0,
+  cust1: randomMinCust(1, 80),
+  cust2: randomMaxCust(1, 100),
+
+  render: function () {
+    randomCookie();
+
+    if (this.cust1 > this.cust2) {
+      this.maxCustomer = this.cust1;
+      this.minCustomer = this.cust2;
+    } else {
+      this.maxCustomer = this.cust2;
+      this.minCustomer = this.cust1;
+    }
+    // ********* DOM MANIPULATION **********
+    // ** STEP 2: CREATE THE ELEMENT
+
+    let articleElem = document.createElement('article');
+    // ** STEP 3: GIVE CONTEXT IF NECESSARY
+    // skip for articleElem
+
+    // ** STEP 4: ADD IT TO THE DOM ---> parent.appendChild(child)
+
+    shopSection.appendChild(articleElem);
+
+    //create h2 element
+
+    let h2Elem = document.createElement('h2');
+    h2Elem.textContent = this.location;
+    articleElem.appendChild(h2Elem);
+
+    //create unordered list
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+
+    for (let i = 0; i < shopHours.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = `${shopHours[i]}: ${cookieAvg[i]} cookies`;
+      ulElem.appendChild(liElem);
+    }
+  }
+};
+parisShop.render();
+
+
+let limaShop = {
+  location: 'Lima',
+  minCustomer: 0,
+  maxCustomer: 0,
+  avgCookieSale: 0,
+  cust1: randomMinCust(1, 80),
+  cust2: randomMaxCust(1, 100),
+
+  render: function () {
+    randomCookie();
+
+    if (this.cust1 > this.cust2) {
+      this.maxCustomer = this.cust1;
+      this.minCustomer = this.cust2;
+    } else {
+      this.maxCustomer = this.cust2;
+      this.minCustomer = this.cust1;
+    }
+    // ********* DOM MANIPULATION **********
+    // ** STEP 2: CREATE THE ELEMENT
+
+    let articleElem = document.createElement('article');
+    // ** STEP 3: GIVE CONTEXT IF NECESSARY
+    // skip for articleElem
+
+    // ** STEP 4: ADD IT TO THE DOM ---> parent.appendChild(child)
+
+    shopSection.appendChild(articleElem);
+
+    //create h2 element
+
+    let h2Elem = document.createElement('h2');
+    h2Elem.textContent = this.location;
+    articleElem.appendChild(h2Elem);
+
+    //create unordered list
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+
+    for (let i = 0; i < shopHours.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = `${shopHours[i]}: ${cookieAvg[i]} cookies`;
+      ulElem.appendChild(liElem);
+    }
+  }
+};
+limaShop.render();
